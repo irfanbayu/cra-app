2c534528cce7de1e159f62f71c35219cfb07ba8a
irfanbayu <88176653+irfanbayu@users.noreply.github.com>
2021-12-13 20:46:21 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import UpdateState from "./components/UpdateState";
+import List from "./components/List";
+
+// import {PlainCSS} from "./components/Styling/PlainCSS"
+// import {SCSSComp} from "./components/Styling/SCSSComp"
+
+function App() {
+  return (
+    <div className="App">
+     
+     <UpdateState/>
+     <List/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FunsState.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FunsState = () => {
+    
+    
+    return (
+        <div>
+            <h1> Hello Dari Function</h1>
+        </div>
+    )
+}
+
+export default FunsState;
